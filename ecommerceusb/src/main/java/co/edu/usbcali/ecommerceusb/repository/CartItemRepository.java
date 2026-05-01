1abfd2167b17611b5210a1d282f6c1be038d2687
jsvianav <151794539+jsvianav@users.noreply.github.com>
2026-05-01 18:54:58 -0500

    feat: add custom query methods to CartItemRepository, OrderItemRepository, PaymentRepository and InventoryRepository

--- ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/repository/CartItemRepository.java
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/repository/CartItemRepository.java
@@ -1,10 +1,11 @@
 package co.edu.usbcali.ecommerceusb.repository;
 
-
 import co.edu.usbcali.ecommerceusb.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+
+    boolean existsByCartIdAndProductId(Integer cartId, Integer productId);
 }
--- ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/repository/OrderItemRepository.java
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/repository/OrderItemRepository.java
@@ -1,10 +1,11 @@
 package co.edu.usbcali.ecommerceusb.repository;
 
-
 import co.edu.usbcali.ecommerceusb.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem,Integer> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+
+    boolean existsByOrderIdAndProductId(Integer orderId, Integer productId);
 }
